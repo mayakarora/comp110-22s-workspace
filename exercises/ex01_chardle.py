bfcb5c3b6f9f9070fed01fd34e52dd38d7d0230c
Maya Arora <mkarora@email.unc.edu>
2022-01-24 23:11:36 -0500

    Finished Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -8,21 +8,34 @@ if len(word) != 5:
     exit()
 else:
     character: str = input("Enter a single character. ")
-    print(character)
 
 if len(character) != 1:
     print("Error: Character must be a single character.")
     exit()
-
 else:
     print("Searching for " + character + " in " + word)
 if character == word[0]:
-    print(character + " found at index 0.")
+    print(character + " found at index 0")
 if character == word[1]:
-    print(character + " found at index 1.")
+    print(character + " found at index 1")
 if character == word[2]:
-    print(character + " found at index 2.")
+    print(character + " found at index 2")
 if character == word[3]:
-    print(character + " found at index 3.")
+    print(character + " found at index 3")
 if character == word[4]:
-    print(character + " found at index 4.")
+    print(character + " found at index 4")
+
+number: int = (word.count(character))
+
+if (word.count(character)) == 0:
+    print("No instances of " + character + " found in " + word)
+if (word.count(character)) == 1:
+    print("1 instance of " + character + " found in " + word)
+if (word.count(character)) == 2:
+    print("2 instances of " + character + " found in " + word)
+if (word.count(character)) == 3:
+    print("3 instances of " + character + " found in " + word)
+if (word.count(character)) == 4:
+    print("4 instances of " + character + " found in " + word)
+if (word.count(character)) == 5:
+    print("5 instances of " + character + " found in " + word)
