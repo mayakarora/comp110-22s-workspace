7c03e64b0daa6cf6a30c4ac7ef0f19c71a207296
Maya Arora <mkarora@email.unc.edu>
2022-02-23 12:40:28 -0500

    main

---
+++ lessons/dictionaries.py
@@ -0,0 +1,17 @@
+"""Demonstrations of dictionary capabilities."""
+
+
+# Declaring the type of a dictionary
+schools: dict[str, int]
+
+
+# Initialize to an empty dictionary
+schools = dict()
+
+# Set a key-value pairing in the dictionary
+schools["UNC"] = 19400
+schools["Duke"] = 6717
+schools["NCSU"] = 26150
+
+# Print a dictionary literal representation
+print(schools)
